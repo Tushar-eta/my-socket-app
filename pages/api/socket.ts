c45022b1b71f17919c0401e4ac3508f33ba03904
Tushar-eta <tusharsharma7409@gmail.com>
2026-02-15 03:04:00 +0530

    removed socket code

--- pages/api/socket.ts
+++
@@ -1,7 +0,0 @@
-import type { NextApiRequest, NextApiResponse } from "next";
-
-// This API route is disabled to prevent duplicate socket servers
-// The main socket server is running in server.js
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  res.status(200).json({ message: "Socket API disabled - using server.js" });
-}
