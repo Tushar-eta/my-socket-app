6857e15c9c6811093451cbee985e2d7a1fea7721
Tushar-eta <tusharsharma7409@gmail.com>
2026-02-15 02:10:13 +0530

    changes

--- pages/api/socket.ts
+++ pages/api/socket.ts
@@ -1,11 +1,7 @@
-import { NextResponse } from "next/server";
+import type { NextApiRequest, NextApiResponse } from "next";
 
 // This API route is disabled to prevent duplicate socket servers
 // The main socket server is running in server.js
-export async function GET() {
-  return NextResponse.json({ message: "Socket API disabled - using server.js" });
-}
-
-export async function POST() {
-  return NextResponse.json({ message: "Socket API disabled - using server.js" });
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  res.status(200).json({ message: "Socket API disabled - using server.js" });
 }
